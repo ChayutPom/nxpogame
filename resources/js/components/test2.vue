3d5941c2fb047f7a67efa57b908404fa0e0d8ed1
ChayutPom <s6030213032@phuket.psu.ac.th>
2021-01-16 15:39:33 +0700

    up

---
+++ resources/js/components/test2.vue
@@ -0,0 +1,28 @@
+<template>
+   <nav
+      class="navbar navbar-light"
+      style="background-color: #e3f2fd; height: 85px"
+    >
+      
+
+    <div class="col-2">
+      <a class="navbar-brand" href="#">Sticky top</a>
+    </div>
+    <div class="col-10">
+ <b-progress
+                      variant="warning"
+                      :max="10"
+                      :value="countDown"
+                      height="25px"
+                    ></b-progress>
+       
+  </div>
+    </nav>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus">
+
+</style>
